27f16c3951df68e97b41e3c186b711ae52f5d1b7
theoddtimi <igbalayewonderful@gmail.com>
2023-02-13 12:35:06 +0100

    trying out my hand once more

---
+++ timi.py
@@ -0,0 +1,4 @@
+print ("Welcome to the Band name generator \n")
+city = input("What is the name of your city \n")
+pet = input ("What is the name of your pet \n")
+print ("Your Band name is " + city + " " + pet)
