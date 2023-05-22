9e517707c5bac642c3d408afc53bba2f24e5f614
theoddtimi <igbalayewonderful@gmail.com>
2023-05-22 14:04:33 +0100

    Day 2 of my udemy course

---
+++ day 2 pyhon.py/assignment1.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+Number_1 = int(two_digit_number [0])
+Number_2 = int(two_digit_number[1])
+two_digit_number = Number_1 + Number_2
+print (two_digit_number)
+input ("")
