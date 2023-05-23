215e60cb4e28b08dacd0a097c351796001889901
theoddtimi <igbalayewonderful@gmail.com>
2023-05-23 21:33:06 +0100

    done on first try

---
+++ day 2 pyhon.py/assignmen3.py
@@ -0,0 +1,17 @@
+# Instructions
+# Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+# It will take your current age as the input and output a message with our time left in this format:
+# You have x days, y weeks, and z months left.
+# Where x, y and z are replaced with the actual calculated numbers.
+# Warning your output should match the Example Output format exactly, even the positions of the commas and full stops.
+
+# Answer
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+y = (90 - int(age)) * 52
+x = (90 - int(age)) * 365 
+z = (90 - int(age)) * 12
+print (f"You have {x} days, {y} weeks, and {z} months left ")
