0d3c25a723377af292150a9e8308af85ccd3eb0b
theoddtimi <igbalayewonderful@gmail.com>
2023-05-24 23:01:36 +0100

    Day 3 first exercise

---
+++ randome scribbles.py
@@ -0,0 +1,44 @@
+
+##DATA TYPES
+# num_char = len(input("what is your name?"))
+# #convert the integer first
+# new_num_char = str(num_char)
+# print("your name has " + new_num_char + " characters")
+# you can use a type check option to know what the data you are working with is, 
+# print(type(num_char)) will show you this <class 'int'>
+# a = str(567)
+# print (type(a))
+#WHEN IT COMES TO PATA TYPES OPERATIONS, PEMDASLR  IS FOLLOWED
+# print (round(11//3))
+# score = 5+1
+# score += 1
+# print (score)
+
+# F-string
+# score = 10
+# height = 1.9
+# winning = True 
+# print(f"your score is {score} your height is {height} you are winning, true or false?: {winning}")
+# print (round(10/3, 2))
+
+
+### Day 3 notes else if operations
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+# put = sign to include 120 in the if category
+if height > 120: 
+  print ("You can ride the roller coaster")
+else:
+  print ("Sorry, you have to grow taller before you can ride")
+
+#   operations include
+# > means greater than
+# < means lesser than
+# >= means greater than or equal to
+# <= means lesser than or equal to
+# == means equal to
+# != means not equal to
+#  note that 2 equal signs should be used during else if variables
+
