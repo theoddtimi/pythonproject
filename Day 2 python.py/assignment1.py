b769dc17d78648205211648ea4d81f7af4413a8f
theoddtimi <igbalayewonderful@gmail.com>
2023-05-24 23:21:14 +0100

    .

--- Day 2 python.py/assignment1.py
+++
@@ -1,6 +0,0 @@
-two_digit_number = input("Type a two digit number: ")
-Number_1 = int(two_digit_number [0])
-Number_2 = int(two_digit_number[1])
-two_digit_number = Number_1 + Number_2
-print (two_digit_number)
-input ("")
--- Day 2 python.py/assignment2.py
+++
@@ -1,7 +0,0 @@
-
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-height_1 = float(height)
-weight_1 = float(weight)
-BMI = weight_1/ height_1 **2
-print (int(BMI))
