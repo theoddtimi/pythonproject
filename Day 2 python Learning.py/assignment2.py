ee8188b3a8be92f1fdbc51b62efeb3406012425c
theoddtimi <igbalayewonderful@gmail.com>
2023-05-24 23:17:02 +0100

    lets get it

---
+++ Day 2 python Learning.py/assignment2.py
@@ -0,0 +1,7 @@
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+height_1 = float(height)
+weight_1 = float(weight)
+BMI = weight_1/ height_1 **2
+print (int(BMI))
