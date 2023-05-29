2dc5df479be8316de722047b88aa3afd74b24d71
theoddtimi <igbalayewonderful@gmail.com>
2023-05-29 22:28:53 +0100

    getting started

---
+++ Day 3 python Learning.py/Assignment 3.py
@@ -0,0 +1,6 @@
+# Write a program that works out whether if a given year is a leap year. 
+# A normal year has 365 days, leap years have 366, with an extra day in February.
+# This is how you work out whether if a particular year is a leap year.
+# on every year that is evenly divisible by 4 
+# **except** every year that is evenly divisible by 100 
+# **unless** the year is also evenly divisible by 400
